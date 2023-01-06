3d2a73e2143c3270ef5dca58652ea15c8a508e24
Haitamy <misterelhaitamy@gmail.com>
2023-01-06 13:19:41 +0100

    class

--- extendsImport/public/js/modules/class.js
+++ extendsImport/public/js/modules/class.js
@@ -1,7 +1,12 @@
-export class Boss {
+export class Perso {
     constructor(nom,pv,pa){
         this.nom = nom
         this.pv=pv
         this.pa=pa
+    }
+}
+export class Boss extends Perso {
+    constructor(nom,pv,pa){
+        super(nom,pv,pa)
     }
 }
