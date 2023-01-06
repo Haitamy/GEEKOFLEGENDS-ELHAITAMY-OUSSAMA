df98f59767c03c25bf326bb1dafb27f5dbe6e1d7
Haitamy <misterelhaitamy@gmail.com>
2023-01-07 00:26:34 +0100

    combat

--- extendsImport/public/js/modules/function.js
+++ extendsImport/public/js/modules/function.js
@@ -154,6 +154,24 @@ export function tour() {
         `${monMage.nom} attaque ${boss.nom} et lui inflige ${monMage.pa} dégats le laissant à ${boss.pv} pv`
       );
       bossAttack();
+    } else if (monArcher.pv == 0 && monGuerrier.pv > 0 && monMage.pv == 0) {
+        boss.pv -= monGuerrier.pv;
+      console.log(
+        `${monGuerrier.nom} attaque ${boss.nom} et lui inflige ${monGuerrier.pa} dégats le laissant à ${boss.pv} pv`
+      );
+      bossAttack();
+    } else if (monArcher.pv == 0 && monGuerrier.pv == 0 && monMage.pv > 0) {
+        boss.pv -= monMage.pv;
+      console.log(
+        `${monMage.nom} attaque ${boss.nom} et lui inflige ${monMage.pa} dégats le laissant à ${boss.pv} pv`
+      );
+      bossAttack();
+    } else if (monArcher.pv > 0 && monGuerrier.pv == 0 && monMage.pv == 0) {
+        boss.pv -= monArcher.pv;
+      console.log(
+        `${monArcher.nom} attaque ${boss.nom} et lui inflige ${monArcher.pa} dégats le laissant à ${boss.pv} pv`
+      );
+      bossAttack();
     }
 
 
