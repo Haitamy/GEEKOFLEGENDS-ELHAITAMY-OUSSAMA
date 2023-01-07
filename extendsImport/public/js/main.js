c86e1e976c743787400342ed95bffc059511f0e8
Haitamy <misterelhaitamy@gmail.com>
2023-01-07 01:47:37 +0100

    main.js

--- extendsImport/public/js/main.js
+++ extendsImport/public/js/main.js
@@ -1,16 +1,4 @@
+import { mesHeros, combat} from "./modules/function.js";
 
-import { tour} from "./modules/function.js";
-import { monGuerrier, monArcher, monMage ,boss } from "./modules/instance.js";
-
-//mesHeros()
-//monAction()
-
-
-tour()
-
-// let tour
-// for (let i = 1; i < 10; i++) {
-//     tour = i
-//     rage(tour)
-// }
-
+mesHeros()
+combat()
